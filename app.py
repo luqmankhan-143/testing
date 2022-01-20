102f0a1fce6b56077b11e5c7e558a210eb6ea15a
luqmankhan-143 <76862331+luqmankhan-143@users.noreply.github.com>
2022-01-20 19:12:48 +0530

    commited

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def login():
 if __name__ == '__main__':
     app.debug=True
     
-    app.run(host='127.0.0.1', port=os.environ.get('PORT', '5000'))
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', '5000'))
     
